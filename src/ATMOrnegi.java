78a21453e18625cfc9715c0bde32d984f130842c
Berkan <sonmezayberkan@gmail.com>
2022-11-30 20:32:33 +0300

    30.11.2022

--- src/ATMOrnegi.java
+++ src/ATMOrnegi.java
@@ -17,4 +17,55 @@ public class ATMOrnegi {
     Bu şekilde basit bir ATM örneği tasarlamış olduk.
      */
 
+    public static void main(String[] args) {
+
+        System.out.println("\t****İSLEMLER****\n" +
+                "    1. Para Çekme\n" +
+                "    2. Para Yatırma\n" +
+                "    3. Bakiye sorgulama\n" +
+                "    4. Kart İade");
+
+        int bakiye= 1000;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nLütfen yapmak istediğiniz işlemi seciniz: ");
+
+        int islem = scan.nextInt();
+
+        switch (islem){
+            case 1:
+                System.out.println("Bakiyeniz: "+ bakiye);
+                System.out.println("Çekmek istediğiniz tutarı giriniz: ");
+                int tutar= scan.nextInt();
+
+                if (tutar>bakiye) {
+                    System.out.println("Yetersiz Bakiye. Yeniden deneyiniz. ");
+                    tutar= scan.nextInt();
+                }
+                bakiye-=tutar;
+                System.out.println("Yeni bakiye: "+ bakiye);
+                break;
+            case 2:
+                System.out.println("Bakiyeniz: "+ bakiye);
+                System.out.println("Yatırmak istediğiniz tutarı giriniz: ");
+                tutar= scan.nextInt();
+                bakiye+=tutar;
+                System.out.println("Yeni bakiye: "+ bakiye);
+                break;
+            case 3:
+                System.out.println("Bakiyeniz: "+ bakiye);
+                break;
+            case 4:
+                System.out.println("Kartınızı almayı unutmayın ");
+                break;
+            default:
+                System.out.println("yanlış işlem yaptınız. ");
+
+
+        }
+
+
+    }
+
+
 }
--- src/AsalSayıBulma.java
+++ src/AsalSayıBulma.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 
 public class AsalSayıBulma {
 
     // Kullanicidian alinan sayinin asal olup olmadigini yazdirin
+
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int sayi =scan.nextInt();
+        int sayac=0;
+
+
+        for (int i = 2; i < sayi; i++) {
+            if(sayi%i==0){
+            sayac++;
+
+            }
+        }
+        if (sayac==0){
+            System.out.println(sayi+" Asal bir sayıdır");
+        }else {
+            System.out.println(sayi+" Asal bir sayı değildir");
+        }
+    }
 }
